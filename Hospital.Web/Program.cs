c981f0913792768833b5cd2c972299a301a835b2
Rahul <rahulkhadake1997@gmail.com>
2024-02-24 19:58:23 +0530

    Add project files.

---
+++ Hospital.Web/Program.cs
@@ -0,0 +1,52 @@
+using Hospital.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Hospital.Utilities;
+using Hospital.Repositories.Implementation;
+using Hospital.Repositories.Interface;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+DataSeeding();
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{Area=Patient}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+void DataSeeding()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbIntializer = scope.ServiceProvider.
+            GetRequiredService<DbInitializer>();
+        dbIntializer.Initialize();
+    }
+}
